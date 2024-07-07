39e1143ae49d870ebec53e8709165f6c07a228ff
beomshin.kim <chzh859@naver.com>
2024-07-07 12:37:24 +0900

    # interpreter 패턴 적용

---
+++ src/interpreter/Main.java
@@ -0,0 +1,21 @@
+package interpreter;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            for (String text : Files.readAllLines(Paths.get("program.txt"))) {
+                System.out.println("text = \"" + text + "\"" );
+                Node node = new ProgramNode();
+                node.parse(new Context(text));
+                System.out.println("node = " + node);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/interpreter/RepeatCommandNode.java
@@ -0,0 +1,21 @@
+package interpreter;
+
+public class RepeatCommandNode extends Node {
+
+    private int number;
+    public Node commandListNode;
+
+    @Override
+    public void parse(Context context) throws ParseException {
+        context.skipToken("repeat");
+        number = context.currentNumber();
+        context.nextToken();
+        commandListNode = new CommandListNode();
+        commandListNode.parse(context);
+    }
+
+    @Override
+    public String toString() {
+        return "[repeat " + number + " " + commandListNode + "]";
+    }
+}
